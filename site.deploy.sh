b24ca7eaa79eb26f263f13b2253bcb05fa0a3cc2
Justin DuJardin <justin@dujardinconsulting.com>
2017-06-04 11:33:59 -0700

    feat: add shell script for deploying github pages site
     - use https://github.com/angular-buch/angular-cli-ghpages and angular-cli prod build

--- site.deploy.sh
+++ site.deploy.sh
@@ -1,2 +1,2 @@
 ng build --prod --base-href "https://justindujardin.github.io/hello-tensors/"
-angular-cli-ghpages --message="deploy site" --no-silent
+./node_modules/.bin/angular-cli-ghpages --message="deploy site" --no-silent
